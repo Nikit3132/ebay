4c6bef7bbd79d7e598fdcda1eb131aa929985dd4
Nikit3132 <prajapatinikit@gmail.com>
2023-07-19 17:15:22 +0530

    first commit

---
+++ src/test/java/ebayshopping/login.java
@@ -0,0 +1,21 @@
+package ebayshopping;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class login {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webDriver.chrome.driver","C:\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.ebay.com/");
+		Thread.sleep(1000);
+		driver.findElement(By.name("_nkw")).sendKeys("mobile");
+		
+		
+	}
+
+}
